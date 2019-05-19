aaab8486efe7d6a2c39868b616626c8a08caeef7
san <test@weblite.ru>
2019-05-19 22:29:54 +0400

    Начало стилифации страницы формы

--- source/js/js.js
+++ source/js/js.js
@@ -1,2 +1,16 @@
-var navMobileButton = document.classlist("main-nav__toogle");
-var
+var navMobileButton = document.querySelector(".main-nav__toogle");
+var mobileMenu = document.querySelector(".main-nav");
+
+mobileMenu.classList.remove("main-nav--nojs");
+
+
+navMobileButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (mobileMenu.classList.contains("main-nav--open")) {
+    mobileMenu.classList.remove("main-nav--open");
+  }
+  else {
+    mobileMenu.classList.add("main-nav--open");
+  }
+
+});
